73dedaf3bcc25b512ac47d9ee1df6aa2118921fa
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-08 10:28:14 +0500

    create model for menus

--- lib/menu_model.dart
+++ lib/menu_model.dart
@@ -10,11 +10,3 @@ class MenuModel{
 
 }
 
-final menuList = [
-  MenuModel(title: 'About us', color: Colors.amber, route: '/about', icon: Icons.info,  ),
-  MenuModel(title: 'Setting', color: Colors.amber, route: '/setting', icon: Icons.settings,),
-  MenuModel(title: 'Search', color: Colors.amber, route: '/search', icon: Icons.search,  ),
-  MenuModel(title: 'Details', color: Colors.amber, route: '/detail', icon: Icons.details,  ),
-  MenuModel(title: 'Verification', color: Colors.amber, route: '/verified', icon: Icons.verified,  ),
-  MenuModel(title: 'Contact us', color: Colors.amber, route: '/contact', icon: Icons.call,  ),
-];
