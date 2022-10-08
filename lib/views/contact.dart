d77befca56c4f6be78ea5882f1cb9be5302e25ef
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-08 10:48:32 +0500

    Update contact.dart

--- lib/views/contact.dart
+++ lib/views/contact.dart
@@ -8,12 +8,9 @@ class contactPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Controller _menuListController =(Get.find());
-
-    final menu = _menuListController.menuList[_menuListController.Gridindex.value];
-
+    final textValue = Get.arguments.toString();
     return Scaffold(
-      appBar: AppBar(title: Text(menu.title),),
+      appBar: AppBar(title: Text(textValue),),
     );
   }
 }
