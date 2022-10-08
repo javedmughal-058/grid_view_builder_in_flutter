d0735fe4b4879fe4703cfe4a4f95caa8880bd58e
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-08 15:41:19 +0500

    Create profile_model.dart

---
+++ lib/Models/profile_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class ProfileModel{
+  final String title;
+  final Color color;
+  final String route;
+  final IconData icon;
+  ProfileModel({required this.title, required this.color, required this.route, required this.icon});
+
+}
+
