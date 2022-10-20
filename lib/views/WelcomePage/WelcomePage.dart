830a3b3ba3ef3ac9f522a8a1ef8aceb78f33bf76
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-20 16:53:17 +0500

    Update WelcomePage.dart

--- lib/views/WelcomePage/WelcomePage.dart
+++ lib/views/WelcomePage/WelcomePage.dart
@@ -12,6 +12,7 @@ class _WelcomePageState extends State<WelcomePage> {
   List images = [
     "w5.jpg",
     "w3.jpg",
+    "w5.jpg",
   ];
   @override
   Widget build(BuildContext context) {
@@ -33,20 +34,38 @@ class _WelcomePageState extends State<WelcomePage> {
             ),
             child: Container(
               margin: const EdgeInsets.only(left: 15, top: 50,right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const AppText(text: 'Trip', color: Colors.white, size: 35,),
-                  const AppText(text: 'Mountains', color: Colors.black45, size: 20,),
-                  const SizedBox(height: 10,),
-                  const AppSmallText(text: 'The journey of a thousand\n miles begins with a single step.' , color: Colors.white, size: 14,),
-                  const SizedBox(height: 20,),
-                  ResponsiveButton(text: 'Welcome' , width: 140,
-                      onPress: (){
-                        print("1");
-                        Get.to(const LoginPage());
-                  }),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const AppText(text: 'Trip', color: Colors.white, size: 35,),
+                      const AppText(text: 'Mountains', color: Colors.black45, size: 20,),
+                      const SizedBox(height: 10,),
+                      const AppSmallText(text: 'The journey of a thousand\n miles begins with a single step.' , color: Colors.white, size: 14,),
+                      const SizedBox(height: 20,),
+                      ResponsiveButton(text: 'Welcome' , width: 140,
+                          onPress: (){
+                            print("1");
+                            Get.to(const LoginPage());
+                      }),
 
+                    ],
+                  ),
+                  Column(
+                    children:List.generate(images.length, (indexDots){
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 3),
+                        width: 6,
+                        height: index==indexDots?25:6,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: index==indexDots ? Colors.amber :  Colors.white,
+                        ),
+                      );
+                    }),
+                  ),
                 ],
               ),
             ),
@@ -94,16 +113,16 @@ class ResponsiveButton extends StatelessWidget {
   final bool? isResponsive;
   final double? width;
   final String text;
-  final Function onPress;
+  final Function() onPress;
   const ResponsiveButton({Key? key,this.isResponsive=false,this.width, required this.text, required this.onPress}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()=>onPress,
+      onTap: onPress,
       child: Container(
         width: width,
-        height: 60,
+        height: 40,
         decoration: BoxDecoration(
           border: Border.all(color: Colors.white),
           borderRadius: BorderRadius.circular(10),
